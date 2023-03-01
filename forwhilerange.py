bc80e8f516bc09808bb14c6cceb0ae1a567dd43a
Alexander Fuchs <alexander.fsoares@outlook.com>
2023-03-01 12:08:45 -0300

    Add files via upload

---
+++ forwhilerange.py
@@ -0,0 +1,36 @@
+# FOR PARA NUMEROS PRIMOS c/ RANGE
+#     a = int(input('Digite um Numero:'))
+#     for num in range(a+1):
+#         div = 0
+#         for x in range(1, num+1):
+#             resto = num % x
+#             #print(x, resto)
+#             if resto == 0:
+#                 div += 1
+#         if div == 2:
+#             print(num)
+#--------------------------------------------------------------------------
+
+
+#WHILE Nota 10
+    # nota = int(input('Entre com a Nota:'))
+    # while nota > 10:
+    #     nota = int(input('A nota não pode ser maior que 10, Digite Novamente:'))
+    # print(nota)
+    # ---------------------------------------------------------------------------
+
+    # a = int(input('Primeiro Bimeste:'))
+    # while a > 10:
+    #     a = int(input('Sua nota não pode ser maior que 10, Digite novamente:'))
+    # b = int(input('Segundo Bimeste:'))
+    # while b > 10:
+    #     b = int(input('Sua nota não pode ser maior que 10, Digite novamente:'))
+    # c = int(input('Terceiro Bimeste:'))
+    # while c > 10:
+    #     c = int(input('Sua nota não pode ser maior que 10, Digite novamente:'))
+    # d = int(input('Quarto Bimeste:'))
+    # while d > 10:
+    #     d = int(input('Sua nota não pode ser maior que 10, Digite novamente:'))
+    # media = ( a + b + c + d) /4
+    # print('Média = {}'.format(media))
+#--------------------------------------------------------------------------
